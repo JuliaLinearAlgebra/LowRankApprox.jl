d9d6e43bbd482da58681eb78b47b7e39d0f1ddab
Richard Slevinsky <Richard.Slevinsky@umanitoba.ca>
2017-05-09 11:53:26 -0500

    fix past approx_eq_eps

--- test/snorm.jl
+++ test/snorm.jl
@@ -14,16 +14,16 @@ for T in (Float32, Float64, Complex64, Complex128)
 
   A = rand(T, m, n)
   nrm = norm(A)
-  @test nrm ≈ snorm(A) atol = rtol*nrm
+  @test ≈(nrm, snorm(A); atol = rtol*nrm)
 
   A = rand(T, n, n)
   A += A'
   nrm = norm(A)
-  @test nrm ≈ snorm(A) atol = rtol*nrm
+  @test ≈(nrm, snorm(A); atol = rtol*nrm)
 
   B = rand(T, size(A))
   nrm = norm(A - B)
-  @test nrm ≈ snormdiff(A, B) atol = rtol*nrm
+  @test ≈(nrm, snormdiff(A, B); atol = rtol*nrm)
 end
 
 toc()
