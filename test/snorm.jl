103e0a3321119bfd07d015c54eb62dae5672d6ab
Sheehan Olver <solver@mac.com>
2018-06-24 15:46:20 +0100

    update CUR but depends on Psvd

--- test/snorm.jl
+++ test/snorm.jl
@@ -12,16 +12,16 @@
       rtol = 100*eps(real(T))
 
       A = rand(T, m, n)
-      nrm = norm(A)
+      nrm = opnorm(A)
       @test ≈(nrm, snorm(A); atol = rtol*nrm)
 
       A = rand(T, n, n)
       A += A'
-      nrm = norm(A)
+      nrm = opnorm(A)
       @test ≈(nrm, snorm(A); atol = rtol*nrm)
 
       B = rand(T, size(A))
-      nrm = norm(A - B)
+      nrm = opnorm(A - B)
       @test ≈(nrm, snormdiff(A, B); atol = rtol*nrm)
     end
   end
