d5d328d958196903f8500daa37bf2cd3d1c47f54
Sheehan Olver <solver@mac.com>
2018-08-08 16:13:19 +0100

    srand -> Random.seed!

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,11 @@ using Compat.Test
 using LowRankApprox
 using Compat.LinearAlgebra, Compat.Random, Compat.SparseArrays
 
-srand(0)
+if VERSION < v"0.7"
+    srand(0)
+else
+    Random.seed!(0)
+end
 
 include("linop.jl")
 include("permute.jl")
